978ccedff8a2997b1235dc62bb0dc1b90a4c95ce
Enrico Di Benedetto <47090069+enrico0014@users.noreply.github.com>
2019-12-24 00:41:23 +0100

    Create bis.c

---
+++ bis.c
@@ -0,0 +1,54 @@
+/*Solving an equation with the bisection method*/
+#include <stdio.h>
+#include <math.h>
+
+float f(float x){
+	return (2*x+1)*(2*x+1)-4*cos(M_PI*x);
+}
+
+int main(){
+	int i=0;
+	float a,b,eps,mp;
+	FILE* fp;
+	
+	/*Input parameters*/
+	printf("Insert the ends of the interval [a,b]:\n");
+	scanf("%f %f",&a,&b);
+	/*First control of existence via mean value theorem*/
+	if(f(a)*f(b)>0){
+		printf("No solution in this interval!\n");
+		return 1;
+	}else if(f(a)==0){
+		printf("The exact solution is %.8f\n",f(a));
+		return 2;
+	}else if(f(b)==0){
+		printf("The exact solution is %.8f\n",f(b));
+		return 3;
+	}
+	
+	printf("Insert the requested number of significative digits:\n");
+	do{
+		printf(">");
+		scanf("%f",&eps);
+		if((eps!=(int)eps || eps<=0)) printf("Insert a positive integer!\n");
+	}while((eps!=(int)eps) || eps<=0);
+	eps= pow(10,-1*eps);
+	
+	fp=fopen("calculation.dat","w");
+	/*Computation of the solution*/
+	while(0.5*fabs(b-a)>eps){
+		mp=0.5*(b+a);
+		fprintf(fp,"%d\t%10.8f\n",++i,mp);
+		if(f(mp)==0){
+			printf("The exact solution is %.8f\n",f(mp));
+			return 4;
+		}
+		else if(f(a)*f(mp)<0) b=mp;
+		else a=mp;
+	}
+	fclose(fp);
+	
+	printf("The found solution is %.8f within an error of %.8f\n",0.5*(b+a),0.5*fabs(b-a));
+	
+	return 0;
+}
